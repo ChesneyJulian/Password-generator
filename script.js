81d9826ad6357aad270b01e51c3143655f302998
Chesney Julian <chesney.julian724@gmail.com>
2023-05-01 12:40:21 -0400

    begin trying to generate random function; unsuccessful but added some content

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 function generatePassword() {
   //  YOUR CODE HERE
 
-var passwordLength= prompt("How many characters would you like your password to contain?");
+  var passwordLength = prompt("How many characters would you like your password to contain?");
 
-    if (passwordLength < 9) {
+      if (passwordLength < 9) {
 
-      alert("Your password must be at least 8 characters and not exceed 128 characters.");
-
-    }   else if (passwordLength > 128) {
-
-      alert("Your password must be at least 8 characters and not exceed 128 characters."); 
-    
-    } 
-
-var passwordLower = confirm("Would you like to include lowercase characters?");
-
-var passwordUpper = confirm("Would you like to include uppercase characters?");
-
-var passwordNumbers = confirm("Would you like to include numbers?");
-
-var passwordSpecial = confirm("Would you like to include special characters?");
-
-      
+        alert("Your password must be at least 8 characters and not exceed 128 characters.");
   
+      }   else if (passwordLength > 128) {
+
+        alert("Your password must be at least 8 characters and not exceed 128 characters.");
+      
+      }  
+      
+  var passwordCriteria = {
+
+      passwordLower: confirm("Would you like to include lowercase characters?"),
+
+      passwordUpper: confirm("Would you like to include uppercase characters?"),
+
+      passwordNumbers: confirm("Would you like to include numbers?"),
+
+      passwordSpecial: confirm("Would you like to include special characters?"),
+
+      // result: " "
+
+      }
+
+  var characters = {
+         lowercase: ("abcdefghijklmnopqrstuvwxyz") ,
+         uppercase: ("ABCDEFGHIJKLMNOPQRSTUVWXYZ") ,
+         numbers: ("0123456789") ,
+         sepcialChars: ("?/>.<,:;!~+-()^{}[]*_") }
+
+
+      if (passwordCriteria = true) {
+        generatePassword(passwordLength); {
+          for (var i = 0; i < length; i++) {
+            characters(Math.floor(Math.random()));
+          }
+        }
+      }
+      return;
     
+      
 
-
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-  var sepcialChars = "?/>.<,:;!~+-()^{}[]*_";
-
+         
+      }
   // var random = 
 
   // var random = Math.floor(Math.random() * numbers.length);
   // return;
-}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -49,5 +68,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
