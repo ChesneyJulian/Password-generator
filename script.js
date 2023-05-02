fa1e1979a2d9cc95d679548544f56c8fdbdc50fb
Chesney Julian <chesney.julian724@gmail.com>
2023-05-02 15:34:56 -0400

    add if statements to add sliced portion characters to chosenCharacters variable if each confirm is true; edit for math function to draw characters from chosenCharacters

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   //  YOUR CODE HERE
-var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#[]/?-$*%" ;
+var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#[/?-$*%&)(+:;<>@^}{~" ;
+var chosenCharacters = "";
+
 // objectives:
 // prompt asking length requirement
 var pwLength = parseInt(prompt("How long would you like your password to be?"));
@@ -14,39 +16,59 @@ var pwLength = parseInt(prompt("How long would you like your password to be?"));
     alert("Password must contain at least 8 characters and no more than 128 characters.");
     return;
   }
+
 // confirm asking to include lowercase
 var includeLower = confirm("Would you like to include lowercase characters?");
+// if true, slice portion of characters containing lowercase and assign to new variable charactersLower; add assignment onto chosenCharacters
+ if (includeLower == true) {
+  var charactersLower = characters.slice(0, 26);
+  console.log(charactersLower);
+  chosenCharacters += charactersLower;
+ }
+ 
 
 // confirm asking to include uppercase
 var includeUpper = confirm("Would you like to include uppercase characters?");
+// if true, slice portion of characters containing uppercase and assign charactersUpper; add assignment onto chosenCharacters
+if (includeUpper == true) {
+  var charactersUpper = characters.slice(26, 52);
+  console.log (charactersUpper);
+  chosenCharacters += charactersUpper;
+}
+
 
 // confirm asking to include numbers
 var includeNum = confirm("Would you like to include numbers?");
+// if true, slice portion of characters containing numbers and assign charactersNum; add assignment onto chosenCharacters
+if (includeNum == true) {
+  var charactersNum = characters.slice(52, 62);
+  console.log (charactersNum);
+  chosenCharacters += charactersNum;
+}
+
 
 // confirm asking to include special characters
 var includeSpecial = confirm("Would you like to include special characters?");
+// if true, slice portion of characters containing special characters and assign characterSpecial; add assignment onto chosenCharacters
+if (includeSpecial == true) {
+  var characterSpecial = characters.slice(62, characters.length);
+  console.log(characterSpecial);
+  chosenCharacters += characterSpecial;
+}
 
+alert("Possible characters chosen for random password generation: " + chosenCharacters);
 
 // random password string generated and shown in output
-// use if statements to slice characters from string according to confirm data
-
-
-
 // referenced https://www.programiz.com/javascript/examples/generate-random-strings
-var charactersLength = characters.length;
-var result = '';
-var length = pwLength;
-for (let i = 0; i < length; i++) {
-  result +=
-  characters.charAt(Math.floor(Math.random() * charactersLength));
 
-  
+var result = '';
+for (let i = 0; i < pwLength; i++) {
+  result +=
+  chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
 }
 return result;
 
-
-
-  }  
+}  
    
 
 
